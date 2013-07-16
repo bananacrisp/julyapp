2bf9f819b3e817b5f8a5338afaf0378621e746bd
Matt Evans <matt@thebetacollective.co.uk>
2013-07-16 13:18:39 +0100

    Pushing to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,12 +16,12 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
-user2 = User.find_or_create_by_email :name => 'Weekly User', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user2.add_role :weekly
-user3 = User.find_or_create_by_email :name => 'Fortnightly User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user3.add_role :fortnightly
-user4 = User.find_or_create_by_email :name => 'Monthly User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user4.add_role :monthly
-user5 = User.find_or_create_by_email :name => 'Fortnightly Switch', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
-user4.add_role :_fortnightly_switch
-puts "users: #{user2.name}, #{user3.name}, #{user4.name}"
+# user2 = User.find_or_create_by_email :name => 'Weekly User', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+# user2.add_role :weekly
+# user3 = User.find_or_create_by_email :name => 'Fortnightly User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+# user3.add_role :fortnightly
+# user4 = User.find_or_create_by_email :name => 'Monthly User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+# user4.add_role :monthly
+# user5 = User.find_or_create_by_email :name => 'Fortnightly Switch', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+# user4.add_role :_fortnightly_switch
+# puts "users: #{user2.name}, #{user3.name}, #{user4.name}"
